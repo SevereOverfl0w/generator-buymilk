dd950e957c2acc4b12160c0d64721506cab0348f
SevereOverfl0w <overfl0w@narna.co>
2014-07-14 14:02:25 +0100

    Update intensely to be much better.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,28 +1,43 @@
 var gulp = require('gulp'),
-    $ = require('gulp-load-plugins')();
+    $ = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    config = {
+        // destinations
+        tmp: '.tmp',
+        app: 'app',
+        dist: 'dist',
+        // globs
+        sass: 'app/**/*.scss',
+    };
 
-
-gulp.task('styles', function() {
-    gulp.src('app/**/*.scss')
-        .pipe($.watch(function(files) {
-            files.pipe($.sass())
-                 .pipe($.autoprefixer())
-                 .pipe($.minifyCss())
-                 .pipe(gulp.dest('./dist'))
-                 .pipe($.livereload());
-        }));
-});
 
 gulp.task('copy', function() {
     gulp.src('app/**/*.html')
         .pipe($.watch())
-        .pipe($.embedlr())
         .pipe(gulp.dest('./dist'))
 });
 
-gulp.task('serve', $.serve({
-    root: ['dist', 'app'],
-    port: 9000
-}));
+gulp.task('sass', function() {
+    return gulp.src(config.sass)
+               .pipe($.plumber())
+               .pipe($.sass())
+               .pipe($.autoprefixer())
+               /*.pipe($.minifyCss())*/
+               .pipe(gulp.dest(config.tmp))
+               .pipe(reload({stream: true}));
+});
 
-gulp.task('default', ['styles', 'copy', 'serve']);
+gulp.task('browser-sync', function() {
+    return browserSync({
+        server: {
+            baseDir: [config.tmp, config.app]
+        }
+    })
+});
+
+gulp.task('watch', function() {
+    $.watch({glob: config.sass, name: 'Sass'}, ['sass']);
+});
+
+gulp.task('default', ['sass', 'copy', 'watch', 'browser-sync']);
